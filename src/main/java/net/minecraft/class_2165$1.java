d37fa85b9c428a47f951835b1339dd74f47cf63a
MFSCelebrate_ <MFSCelebrate@163.com>
2026-06-29 14:35:52 +0800

    Add files via upload

---
+++ src/main/java/net/minecraft/class_2165$1.java
@@ -0,0 +1,24 @@
+//
+// Decompiled by Jadx - 991ms
+//
+package net.minecraft;
+
+class class_2165$1 implements class_2165 {
+    class_2165$1() {
+    }
+
+    public void method_43496(class_2561 class_2561Var) {
+    }
+
+    public boolean method_9200() {
+        return false;
+    }
+
+    public boolean method_9202() {
+        return false;
+    }
+
+    public boolean method_9201() {
+        return false;
+    }
+}
---
+++ src/main/java/net/minecraft/class_2165.java
@@ -0,0 +1,20 @@
+//
+// Decompiled by Jadx - 786ms
+//
+package net.minecraft;
+
+public interface class_2165 {
+    public static final class_2165 field_17395 = new class_2165$1();
+
+    void method_43496(class_2561 class_2561Var);
+
+    boolean method_9200();
+
+    boolean method_9201();
+
+    boolean method_9202();
+
+    default boolean method_36320() {
+        return false;
+    }
+}
